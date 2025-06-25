733ac0cd33b33ece39a4a7bf8caa19006eb8e297
Vhon Michael <vhontabalan@gmail.com>
2025-06-25 19:54:05 +0800

    adding a databse context

---
+++ Kards.NET/DBContext/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Kards.NET.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kards.NET.DBContext;
+
+public class ApplicationDbContext : DbContext
+{
+    DbSet<Decks> Decks { get; set; }
+    DbSet<Cards> Cards { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=Kards.db");
+    }
+}
