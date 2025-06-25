9a8cf3156c3e87649f8a3db43a6e1945c4f6da48
Vhon Michael <vhontabalan@gmail.com>
2025-06-25 10:33:33 +0800

    added some addional properties

--- Kards.NET/Models/Decks.cs
+++ Kards.NET/Models/Decks.cs
@@ -1,8 +1,13 @@
-﻿namespace Kards.NET.Models;
+﻿using System;
+
+namespace Kards.NET.Models;
 
 public class Decks
 {
     public int Id { get; set; }
-    public required string Name { get; set; }
-    public Cards? Cards { get; set; }
+    public required string DeckName { get; set; }
+    public string? DeckDescription { get; set; }
+    public DateTime CreationDate { get; set; } = DateTime.Now;
+    public DateTime LastAcess { get; set; }
+    
 }
