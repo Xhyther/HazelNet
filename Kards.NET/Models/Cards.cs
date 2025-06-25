52b6bb0b230153ec631a59bd1c2d34b364eaea06
Vhon Michael <vhontabalan@gmail.com>
2025-06-25 10:23:24 +0800

    Adding simple Models for Decks and Cards

---
+++ Kards.NET/Models/Cards.cs
@@ -0,0 +1,8 @@
+﻿namespace Kards.NET.Models;
+
+public class Cards
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+}
---
+++ Kards.NET/Models/Decks.cs
@@ -0,0 +1,8 @@
+﻿namespace Kards.NET.Models;
+
+public class Decks
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public Cards? Cards { get; set; }
+}
