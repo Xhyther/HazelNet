7874d70c3bfae13b7111891257a246df04061d56
Vhon Michael <vhontabalan@gmail.com>
2025-06-28 23:18:23 +0800

    some trial hardcoded decks to views

--- Kards.NET/ViewModels/DeckViewModel.cs
+++ Kards.NET/ViewModels/DeckViewModel.cs
@@ -1,6 +1,33 @@
-﻿namespace Kards.NET.ViewModels;
+﻿using System;
+using System.Collections.ObjectModel;
+using Kards.NET.Models;
+
+namespace Kards.NET.ViewModels;
 
 public class DeckViewModel : ViewModelBase
 {
-   
+   public ObservableCollection<Decks> Decks { get; set; }
+
+   public DeckViewModel()
+   {
+       Decks = new ObservableCollection<Decks>
+       {
+           new Decks
+           {
+               Id = 1,
+               DeckName = "Laptops",
+               DeckDescription = "Collection of Laptops and Specs",
+               CreationDate = DateTime.Now,
+               LastAcess = DateTime.Now,
+           },
+           new Decks
+           {
+               Id = 2,
+               DeckName = "Pokedex",
+               DeckDescription = "Collection of Pokemon Cards",
+               CreationDate = DateTime.Now,
+               LastAcess = DateTime.Now,
+           }
+       };
+   }
 }
