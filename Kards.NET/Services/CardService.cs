6b0bc073c65fad53420b27d6f9e9c959174759f2
Vhon Michael <vhontabalan@gmail.com>
2025-06-30 22:12:53 +0800

    added CardService.cs with basic get configs

---
+++ Kards.NET/Services/CardService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kards.NET.DBContext;
+using Kards.NET.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kards.NET.Services;
+
+public class CardService
+{
+    private readonly ApplicationDbContext _db;
+
+    public CardService(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    
+    //List All cards
+    public async Task<List<Cards>> GetAllCardsAsync() => await _db.Cards.ToListAsync();
+
+    //List all Cards by DeckId
+    public async Task<List<Cards>> GetAllCardsByDeckAsync(int DeckID)
+    {
+        return await _db.Cards.Where(x => x.DeckId == DeckID).ToListAsync();
+    }
+}
