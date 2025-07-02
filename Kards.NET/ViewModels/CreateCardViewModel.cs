a5ab814b70c36d612dea40f2fa9b5641213d52a3
Vhon Michael <vhontabalan@gmail.com>
2025-07-02 22:37:55 +0800

    Constructor based DI for the CreateCardViewModel.cs

--- Kards.NET/ViewModels/CreateCardViewModel.cs
+++ Kards.NET/ViewModels/CreateCardViewModel.cs
@@ -1,6 +1,14 @@
-﻿namespace Kards.NET.ViewModels;
+﻿using Kards.NET.Services;
+
+namespace Kards.NET.ViewModels;
 
 public class CreateCardViewModel
 {
+    private readonly DeckService _deckService;
     
+    public CreateCardViewModel(DeckService deckService)
+    {
+        //Dependency Injection
+        _deckService = deckService;
+    }
 }
