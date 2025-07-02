47953c04829ea738051e3b3587c1c552547c140a
Vhon Michael <vhontabalan@gmail.com>
2025-07-02 23:40:32 +0800

    Set the Data Context to the DI DeckViewModel.cs

--- Kards.NET/Views/DeckView.axaml.cs
+++ Kards.NET/Views/DeckView.axaml.cs
@@ -1,6 +1,8 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Kards.NET.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Kards.NET.Views;
 
@@ -9,5 +11,6 @@ public partial class DeckView : UserControl
     public DeckView()
     {
         InitializeComponent();
+        DataContext = App.Services.GetRequiredService<DeckViewModel>();
     }
 }
