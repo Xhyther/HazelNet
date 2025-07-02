5cdc975e208cf2934e66485dd0867df2e9cd0657
Vhon Michael <vhontabalan@gmail.com>
2025-07-02 23:39:02 +0800

    Made a window for showing the control CreateCardView for Card input

---
+++ Kards.NET/Views/CreateCardWindow.axaml.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Kards.NET.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kards.NET.Views;
+
+public partial class CreateCardWindow : Window
+{
+    public CreateCardWindow()
+    {
+        InitializeComponent();
+        DataContext = App.Services.GetRequiredService<CreateCardViewModel>();
+    }
+}
