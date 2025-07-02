d36ba0af898280f13e0a249761e91cbc22f8b1cd
Vhon Michael <vhontabalan@gmail.com>
2025-07-02 17:34:11 +0800

    Created a CreateCard Window for card details input

---
+++ Kards.NET/Views/CreateCardView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Kards.NET.Views;
+
+public partial class CreateCardView : UserControl
+{
+    public CreateCardView()
+    {
+        InitializeComponent();
+    }
+}
