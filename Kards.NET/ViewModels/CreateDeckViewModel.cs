790ac8392a305e6bb00097fac05e073eeec6e6a7
Vhon Michael <vhontabalan@gmail.com>
2025-07-07 00:43:47 +0800

    Created Deck components for deck input

---
+++ Kards.NET/ViewModels/CreateDeckViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Kards.NET.Services;
+
+namespace Kards.NET.ViewModels;
+
+public class CreateDeckViewModel
+{
+    private readonly DeckService _deckService;
+
+    public CreateDeckViewModel(DeckService deckService)
+    {
+        _deckService = deckService;
+    }
+}
