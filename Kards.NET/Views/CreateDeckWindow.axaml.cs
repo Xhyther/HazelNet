a21f9c250a9e5a817203a154e50d3e56031bb7c7
Vhon Michael <vhontabalan@gmail.com>
2025-07-07 00:44:35 +0800

    Created Deck creation window with the data context set as the Service of CreateCardViewModel.cs

---
+++ Kards.NET/Views/CreateDeckWindow.axaml.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia.Controls;
+using Kards.NET.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kards.NET.Views;
+
+public partial class CreateDeckWindow : Window
+{
+    public CreateDeckWindow()
+    {
+        DataContext = App.Services.GetRequiredService<CreateDeckViewModel>();
+        InitializeComponent();
+    }
+}
