8413e74e4bef3651ec5fa42b32ce591d1ab71aea
Vhon Michael <vhontabalan@gmail.com>
2025-07-15 00:18:18 +0800

    Implemented the corresponding event in the Window

--- Kards.NET/Views/CreateDeckWindow.axaml.cs
+++ Kards.NET/Views/CreateDeckWindow.axaml.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Kards.NET.ViewModels;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,7 +9,12 @@ public partial class CreateDeckWindow : Window
 {
     public CreateDeckWindow()
     {
-
         InitializeComponent();
+        DataContext = App.Services.GetRequiredService<CreateDeckWindowViewModel>();
+    }
+
+    private void CancelDeckCreation(object? sender, RoutedEventArgs e)
+    {
+        this.Close();
     }
 }
