d0ff856fea58b8d774e0facfe1bd3bfda0cc5612
Vhon Michael <vhontabalan@gmail.com>
2025-07-18 01:21:12 +0800

    added ApplicationDBContextFactory.cs for EFCore

---
+++ Kards.NET/DBContext/ApplicationDBContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Kards.NET.DBContext;
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+        // IMPORTANT: Use the same connection string your app uses
+        optionsBuilder.UseSqlite("Data Source=kards.db");
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
