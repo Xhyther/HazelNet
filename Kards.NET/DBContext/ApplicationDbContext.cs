2991928076b07805ba781548e6d4f1869aff9d2c
Vhon Michael <vhontabalan@gmail.com>
2025-07-19 01:38:56 +0800

    added some debugging for figuring out databse location

--- Kards.NET/DBContext/ApplicationDbContext.cs
+++ Kards.NET/DBContext/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Kards.NET.Models;
+﻿using System;
+using System.IO;
+using Kards.NET.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Kards.NET.DBContext;
@@ -7,9 +9,16 @@ public class ApplicationDbContext : DbContext
 {
     public DbSet<Decks> Decks { get; set; }
     public DbSet<Cards> Cards { get; set; }
-    
+
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options) {}
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var dbPath = Path.GetFullPath("Kards.db");
+        Console.WriteLine($"Database location: {dbPath}");
+        optionsBuilder.UseSqlite($"Data Source={dbPath}");
+    }
+    
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
