65afb5a8478ce8cda5e2967c4bfc6663e59fa37a
Vhon Michael <vhontabalan@gmail.com>
2025-07-18 23:21:31 +0800

    Made a simple Enter Deck Creation function that saves teh Deck Entered

--- Kards.NET/ViewModels/CreateDeckWindowViewModel.cs
+++ Kards.NET/ViewModels/CreateDeckWindowViewModel.cs
@@ -1,13 +1,41 @@
-﻿using Kards.NET.Services;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Kards.NET.Models;
+using CommunityToolkit.Mvvm.Input;
+using Kards.NET.Services;
 
 namespace Kards.NET.ViewModels;
 
-public class CreateDeckWindowViewModel
+public partial class CreateDeckWindowViewModel : ObservableObject
 {
+    public Action CloseWindow { get; set; }
+    
     private readonly DeckService _deckService;
-
+    
+    [ObservableProperty]
+    private string _deckName =  string.Empty;
+    
     public CreateDeckWindowViewModel(DeckService deckService)
     {
         _deckService = deckService;
+    }
+
+    [RelayCommand]
+    public async Task EnterDeckCreation()
+    {
+        if (!string.IsNullOrWhiteSpace(DeckName))
+        {
+            var newDeck = new Decks
+            {
+                DeckName = DeckName,
+                LastAcess = DateTime.Now
+            };
+            Console.WriteLine("New Deck Created!");
+            await _deckService.AddDeckAsync(newDeck);
+            CloseWindow?.Invoke();
+            Console.WriteLine("Closing......");
+        }
     }
 }
