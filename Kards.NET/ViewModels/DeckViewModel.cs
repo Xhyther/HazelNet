76f6ad3450e4b6a1a7644f798ebe40bcc53cef7f
Vhon Michael <vhontabalan@gmail.com>
2025-07-22 00:35:29 +0800

    Made it so that when the entering new deck information it dynamically refreshesh the page

--- Kards.NET/ViewModels/DeckViewModel.cs
+++ Kards.NET/ViewModels/DeckViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.Input;
 using Kards.NET.Models;
@@ -8,13 +9,12 @@ using Kards.NET.Views;
 
 namespace Kards.NET.ViewModels;
 
-public partial class DeckViewModel : ViewModelBase
+public partial class DeckViewModel : ViewModelBase, INotifyPropertyChanged
 {
 
    private readonly DeckService _deckService;
    
-   public ObservableCollection<Decks> Decks { get; set; } = new();
-   
+   public ObservableCollection<Decks> Decks { get; set; } = new ObservableCollection<Decks>();
 
    //Overload Constructor for testing
    
@@ -28,9 +28,8 @@ public partial class DeckViewModel : ViewModelBase
 
    public async Task LoadAllDecks()
    {
-      var decks = new ObservableCollection<Decks>(await _deckService.GetAllDecksAsync());
       Decks.Clear();
-      foreach (var deck in decks)
+      foreach (var deck in await _deckService.GetAllDecksAsync())
          Decks.Add(deck); // No manual OnPropertyChanged needed
    }
    
@@ -39,6 +38,7 @@ public partial class DeckViewModel : ViewModelBase
    public void CreateNewDeckCommand()
    {
       var window = new CreateDeckWindow();
+      window.Closed += async (s, e) => await LoadAllDecks();
       window.Show();
    }
    
