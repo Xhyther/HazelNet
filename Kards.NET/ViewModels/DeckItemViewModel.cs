ddd3491dbdcaea8a4a36a1f3d4e27d9e7ef60fd6
Vhon Michael <vhontabalan@gmail.com>
2025-08-03 23:27:56 +0800

    Creating anm ItemVViewModel for exposing properties of Deck while making them strings and having access tio parent class

---
+++ Kards.NET/ViewModels/DeckItemViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Kards.NET.Models;
+
+namespace Kards.NET.ViewModels;
+
+public class DeckItemViewModel
+{
+    public Decks Decks { get; }
+    public DeckViewModel Parent { get; }
+
+    public DeckItemViewModel(Decks decks, DeckViewModel parent)
+    {
+        Decks = decks;
+        Parent = parent;
+    }
+    
+    
+    public string DeckName => Decks.DeckName;
+    public string NumberOfCards => "Cards: " + Decks.NumberOfCards.ToString();
+    public string LastAccess => "Last Studied: " + Decks.LastAcess.ToString();
+}
