2d996df5fa70774ef77fbb63d5b51c7182f39ed7
Vhon Michael <vhontabalan@gmail.com>
2025-08-16 19:21:58 +0800

    Actual class implementaion of the interface INavigationService

---
+++ Kards.NET/Services/NavigationService.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Kards.NET.ViewModels;
+
+namespace Kards.NET.Services;
+
+public partial class NavigationService: ObservableObject,  INavigationService
+{
+    [ObservableProperty]
+    private ViewModelBase _currentPage;
+    
+    [ObservableProperty]
+    private string _pageTitle;
+    
+    
+    public void NavigateTo(ViewModelBase vm, string title)
+    {
+        CurrentPage = vm;
+        PageTitle = title;
+    }
+}
