91c418ca3ae55306c71c4400c2df55c34854e86f
Vhon Michael <vhontabalan@gmail.com>
2025-08-25 00:08:33 +0800

    Made another constructor with parameters Viewmodel and Deck instance

--- Kards.NET/Views/EditDeckWindow.axaml.cs
+++ Kards.NET/Views/EditDeckWindow.axaml.cs
@@ -1,6 +1,7 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using Kards.NET.Models;
+using Kards.NET.ViewModels;
 
 namespace Kards.NET.Views;
 
@@ -9,5 +10,12 @@ public partial class EditDeckWindow : Window
     public EditDeckWindow()
     {
         InitializeComponent();
+    }
+
+    public EditDeckWindow(EditDeckWindowViewModel viewModel, Decks deck)
+    {
+        InitializeComponent();
+        viewModel.Decks = deck;
+        DataContext = viewModel;
     }
 }
