719acaf660462aecbe872d38712b8ccbf1351a42
Vhon Michael <vhontabalan@gmail.com>
2025-08-25 00:10:16 +0800

    implemented some basic properties for testing

--- Kards.NET/ViewModels/EditDeckWindowViewModel.cs
+++ Kards.NET/ViewModels/EditDeckWindowViewModel.cs
@@ -1,6 +1,14 @@
-﻿namespace Kards.NET.ViewModels;
+﻿using Kards.NET.Models;
+
+namespace Kards.NET.ViewModels;
 
 public class EditDeckWindowViewModel : ViewModelBase
 {
+    public Decks Decks {get;set;}
+    public string DeckName  => Decks.DeckName;
+    
+   
+    
+    
     
 }
