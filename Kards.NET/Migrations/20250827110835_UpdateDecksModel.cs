4496b38cfb6e0f8c414db92015741af04093a846
Vhon Michael <vhontabalan@gmail.com>
2025-08-27 22:43:31 +0800

    Updated the Database

---
+++ Kards.NET/Migrations/20250827110835_UpdateDecksModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kards.NET.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDecksModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumberOfCards",
+                table: "Decks");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfCards",
+                table: "Decks",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
---
+++ Kards.NET/Migrations/20250827122621_Removed Deck Count in DB literally.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kards.NET.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedDeckCountinDBliterally : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
