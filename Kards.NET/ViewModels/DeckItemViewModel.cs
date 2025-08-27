bf21a7b146fc1a27dcd96e9df3cad474109196c2
Vhon Michael <vhontabalan@gmail.com>
2025-08-27 21:19:33 +0800

    Gettingt the Number of Cards From Deck property to Manually Count

--- Kards.NET/ViewModels/DeckItemViewModel.cs
+++ Kards.NET/ViewModels/DeckItemViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Kards.NET.Models;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Kards.NET.Models;
 
 namespace Kards.NET.ViewModels;
 
@@ -6,15 +7,18 @@ public class DeckItemViewModel
 {
     public Decks Deck { get; }
     public DeckViewModel Parent { get; }
+    
+    public string DeckName => Deck.DeckName;
+    
+    public string NumberOfCards => Deck.Cards.Count + " cards"; 
+
+    public string LastAccess => "Last Studied: " + Deck.LastAcess;
 
     public DeckItemViewModel(Decks decks, DeckViewModel parent)
     {
         Deck = decks;
         Parent = parent;
+        
     }
     
-    
-    public string DeckName => Deck.DeckName;
-    public string NumberOfCards =>  Deck.NumberOfCards.ToString() + " cards";
-    public string LastAccess => "Last Studied: " + Deck.LastAcess.ToString();
 }
