bd118e26e2837c8d7825b4f060007a89f112fb16
Vhon Michael <vhontabalan@gmail.com>
2025-08-27 21:19:57 +0800

    removed the Number of Cards Model property

--- Kards.NET/Models/Decks.cs
+++ Kards.NET/Models/Decks.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+
 
 namespace Kards.NET.Models;
 
@@ -9,8 +9,8 @@ public class Decks
     public int Id { get; set; }
     public string DeckName { get; set; }
     public DateTime CreationDate { get; set; } = DateTime.Now;
-    public DateTime LastAcess { get; set; }
+    public DateTime LastAcess { get; set; } = DateTime.Now;
     
     public ICollection<Cards> Cards { get; set; } = new List<Cards>();
-    public int NumberOfCards { get; set; }
+    
 }
