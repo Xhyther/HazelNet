f3002513665a0e0c9c0b49a51649e18ea02f5fb1
Vhon Michael <vhontabalan@gmail.com>
2025-09-25 03:08:42 +0800

    Created a DeckItemViewModel for the Item repeater accesing the methods

---
+++ Kards.NET/ViewModels/EditDeckItemViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Kards.NET.Models;
+
+namespace Kards.NET.ViewModels;
+
+
+public class EditDeckItemViewModel
+{
+    public  EditDeckWindowViewModel Parent { get; set; }
+    public Cards Card { get; set; }
+    
+    public EditDeckItemViewModel(EditDeckWindowViewModel editDeckWindowViewModel, Cards cards)
+    {
+        Parent = editDeckWindowViewModel;
+        Card = cards;
+    }
+}
