5953fdd55bbd13fb15f1c0471f5c7b6f17497711
Vhon Michael <vhontabalan@gmail.com>
2025-09-27 21:27:35 +0800

    made a simple study mode window for prototyping

---
+++ Kards.NET/Views/StudyWindow.axaml.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Kards.NET.Models;
+using Kards.NET.ViewModels;
+
+namespace Kards.NET.Views;
+
+public partial class StudyWindow : Window
+{
+    public StudyWindow()
+    {
+        InitializeComponent();
+        DataContext = new StudyWindowViewModel();
+    }
+    
+    public StudyWindow(StudyWindowViewModel vm)
+    {
+        InitializeComponent();
+        DataContext = vm;
+    }
+    
+    public StudyWindow(StudyWindowViewModel vm, Decks d)
+    {
+        InitializeComponent();
+        DataContext = vm;
+        vm.LoadCard(d);
+    }
+}
