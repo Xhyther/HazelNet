2f929cdd26b16176de1541e373c85d40c0eed2bd
Vhon Michael <vhontabalan@gmail.com>
2025-09-27 21:27:52 +0800

    added StudyWindowViewModel.cs

---
+++ Kards.NET/ViewModels/StudyWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using Kards.NET.Models;
+
+namespace Kards.NET.ViewModels;
+
+public class StudyWindowViewModel
+{
+    public Decks deck {get; set;}
+    public ObservableCollection<Cards> Card { get; set; }
+    public string currentDisplay { get; set; }
+
+    public StudyWindowViewModel()
+    {
+        currentDisplay = "sample front";
+    }
+
+    public void LoadCard(Decks d)
+    {
+        deck = d;
+    }
+    
+}
